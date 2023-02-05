ad195d07f7a27a9be71dc2d3ef9a395e44634227
Mehmet Ali Kisacik <mehmetali.kisacik@outlook.com>
2023-02-05 17:20:03 +0300

    Add app coordinator

---
+++ Beam Analyzer/App Coordinator/AppCoordinator.swift
@@ -0,0 +1,26 @@
+//
+//  AppCoordinator.swift
+//  Beam Analyzer
+//
+//  Created by Mehmet Ali Kısacık on 5.02.2023.
+//
+
+import UIKit
+
+final class AppCoordinator {
+
+    private let window: UIWindow
+
+    init(window: UIWindow) {
+        self.window = window
+    }
+
+    func start(){
+        let splashScreenVC = SplashScreenViewController(nibName: String(describing: SplashScreenViewController.self), bundle: nil)
+        let navigationController = UINavigationController(rootViewController: splashScreenVC)
+        navigationController.setNavigationBarHidden(true, animated: false)
+        window.rootViewController = navigationController
+        window.makeKeyAndVisible()
+    }
+
+}
