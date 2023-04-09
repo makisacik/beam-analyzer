833939cb9fc7ddf06017d4a585c397be134736a7
Mehmet Ali Kısacık <71513921+makisacik@users.noreply.github.com>
2023-04-09 22:01:47 +0300

    Revert "Feature/messaging"

--- Beam Analyzer/Menu View/MenuViewController.swift
+++ Beam Analyzer/Menu View/MenuViewController.swift
@@ -13,16 +13,6 @@ final class MenuViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        showLoadingAnimation()
-        
-        UserManager.shared.fetchCurrentUser { error in
-            if error != nil {
-                self.showError(title: "Error", message: "User cannot be fetched")
-            }
-            DispatchQueue.main.async {
-                self.hideLoadingAnimation()
-            }
-        }
         setupViews()
     }
     
